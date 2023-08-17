ccc204b8617b0af9c71cf0ab9768ede3cf8a7530
Aquobus <60034276+Aquobus@users.noreply.github.com>
2023-08-17 09:31:41 +0300

    Added __main__.py and command.py

---
+++ __main__.py
@@ -0,0 +1,37 @@
+import os
+import asyncio
+from aiogram import Bot, Dispatcher, Router, types
+from aiogram.filters import Command
+from aiogram.types import Message
+from command import COMMANDS_DATA
+from . import botsTokens as tokens
+
+TOKEN      = tokens.USER_TOKEN
+dp         = Dispatcher()
+bot        = Bot(TOKEN, parse_mode="HTML")
+
+#@dp.message()
+#async def default(message: Message) -> None:
+    #await message.answer('Выбрано неверное действие')
+
+#@dp.message()
+#async def do(message: Message, answer_before: str, answer_after: str, func) -> None:
+
+
+@dp.message(Command(commands='help'))
+async def commands(message: Message) -> None:
+    for command, description in COMMANDS_DATA.items():
+        await bot.send_message(message.from_user.id, f'{command}, {description}')
+
+@dp.message(Command(commands='start'))
+async def command_start_handler(message: Message) -> None:
+    await message.answer(f"Привет, <b>{message.from_user.full_name}</b>! Я Бот, созданный @Aquobus и @dany2k")
+
+async def main() -> None:
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except (KeyboardInterrupt, SystemExit) as e: 
+        print(f'Bot stopped with reason: {e}!')
